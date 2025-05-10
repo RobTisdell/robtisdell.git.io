0780a46c5c07e5276f76a3aff405661ede56ea9a
RobTisdell <robjtsdell@gmail.com>
2025-05-10 02:55:41 -0400

    Update
    Update

--- scripts/cssselector.js
+++
@@ -1,8 +0,0 @@
-function selectCSS {
-    if ( document.documentElement.clientWidth > 900 ) {
-		ocument.getElementsByTagName("head")[0].insertAdjacentHTML( "beforeend", "<link rel='stylesheet' href='desktop_style.css' />");
-	}
-	else {
-        document.getElementsByTagName("head")[0].insertAdjacentHTML( "beforeend", "<link rel='stylesheet' href='mobile_style.css />");
-    }
-}
