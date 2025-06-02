ee6ea066f49676c98fda41d32c0966e65da6c250
RobTisdell <robjtsdell@gmail.com>
2025-06-02 02:15:12 -0400

    update
    update

--- scripts/currentstaff.js
+++ scripts/currentstaff.js
@@ -1,6 +1,11 @@
-async function countCurrentStaff('https://robtisdell.github.io/robtisdell.git.io/scripts/staff.json', IsActive, true) {
+const staffSource = 'https://robtisdell.github.io/robtisdell.git.io/scripts/staff.json';
+const attributeName = 'IsActive';
+const attributeValue = true;
+
+
+async function countCurrentStaff(staffSource, attributeName, attributeValue) {
     try {
-        const response = await fetch('https://robtisdell.github.io/robtisdell.git.io/scripts/staff.json');
+        const response = await fetch(staffSource);
         const jsonData = await response.json();
 
         if (!Array.isArray(jsonData)) {
@@ -8,10 +13,10 @@ async function countCurrentStaff('https://robtisdell.github.io/robtisdell.git.io
         }
 
         const staffCount = jsonData.filter(obj =>
-            obj.hasOwnProperty(IsActive) && obj[IsActive] === true
+            obj.hasOwnProperty(attributeName) && obj[attributeName] === attributeValue
         );
 
-        return countStaff.length;
+        return staffCount.length;
 
     } catch (error) {
         console.error("Error:", error);
