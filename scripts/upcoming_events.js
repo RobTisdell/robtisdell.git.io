64acf42a7634fc7b135ccb8b8686ec507431b96c
RobTisdell <robjtsdell@gmail.com>
2026-05-21 22:28:40 -0400

    Cleaning up upcoming events.............

--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -157,7 +157,7 @@
 
 		return `
 			<div class="event_boxes" id="event-${event.ID}">
-				<img src="img/events/${event.Image}" alt="${event.Name} image">
+				<div class="event_images"><img src="img/events/${event.Image}" alt="${event.Name} image"></div>
 				<p class="event_details"><strong>Type of event:</strong> ${event.Type}</p>
 				<p class="event_details"><strong>${dateLabel}:</strong> ${dateDisplay}</p>
 				${groupedHtml}
